91f746422e5e7e59d98d4ff13414134fb11976ce
Manu <tarriosantos@gmail.com>
2023-12-06 21:25:20 +0100

    fix dia1 complete

--- dia1/dia1.py
+++ dia1/dia1.py
@@ -1,5 +1,5 @@
 #Open input
-filename = 'input.txt'
+filename = 'input_test.txt'
 with open(filename) as file:
     lines = [line.rstrip() for line in file]
 #Just test the input
@@ -7,26 +7,79 @@ with open(filename) as file:
 #  print(line) 
 
 #Make things
+
+digits = {
+"one" : 1,
+"two" : 2,
+"three" : 3,
+"four" : 4,
+"five" : 5,
+"six" : 6,
+"seven" : 7,
+"eight" : 8,
+"nine" : 9,
+"0" : 0,
+"1" : 1,
+"2" : 2,
+"3" : 3,
+"4" : 4,
+"5" : 5,
+"6" : 6,
+"7" : 7,
+"8" : 8,
+"9" : 9
+}
+
 result = 0
+
 for line in lines:
-  partial_result = 0 
+  found = 0
+  partial_result = ""
+  miniresult = 0
+  print(line)
   for i in range(0, len(line), 1):
     try:
       string_int = int(line[i])
       result += string_int * 10
-      partial_result += string_int * 10
       break
     except ValueError:
+      partial_result = partial_result + line[i]
+      keys = digits.keys()
+      for key in keys:
+        if partial_result.__contains__(key) :
+          string_int = digits[key]
+          found = 1
+        break
+      if found == 1 :
+        result += string_int * 10
+        found = 0
+        miniresult += string_int * 10
+        break
+
       continue
-      
+
+  found = 0
+  partial_result = ""
   for j in range (len(line)-1, -1, -1):
     try:
       string_int = int(line[j])
       result += string_int 
-      partial_result += string_int
       break
     except ValueError:
+      partial_result = line[j] + partial_result
+      keys = digits.keys()
+      for key in keys:
+        if partial_result.__contains__(key) :
+          string_int = digits[key]
+          found = 1
+        break
+      if found == 1 :
+        result += string_int
+        found = 0
+        miniresult += string_int
+        break
+      
       continue
-  print(line + " " + str(j) + " " + str(partial_result))
+  print(miniresult)
 #Print result         
 print(result)
