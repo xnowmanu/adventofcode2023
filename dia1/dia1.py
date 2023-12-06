86a2659f04c388b3a2cb945780ef30061f8562a5
Manu <tarriosantos@gmail.com>
2023-12-06 20:55:25 +0100

    fix dia1 complete

--- dia1/dia1.py
+++ dia1/dia1.py
@@ -1,23 +1,32 @@
 #Open input
-filename = 'input_test.txt'
+filename = 'input.txt'
 with open(filename) as file:
     lines = [line.rstrip() for line in file]
 #Just test the input
-for line in lines:
-  print(line) 
+#for line in lines:
+#  print(line) 
 
 #Make things
 result = 0
 for line in lines:
-  for i in range(0, len(line)-1, 1):
-    if int(line[i]) != 0: 
-      result += int(line[i]) * 10
+  partial_result = 0 
+  for i in range(0, len(line), 1):
+    try:
+      string_int = int(line[i])
+      result += string_int * 10
+      partial_result += string_int * 10
       break
-  for j in range (len(line)-1, 0, -1):
-    
-    if int(line[j]) != 0:
-      result += int(line[j])
+    except ValueError:
+      continue
+      
+  for j in range (len(line)-1, -1, -1):
+    try:
+      string_int = int(line[j])
+      result += string_int 
+      partial_result += string_int
       break
-  
+    except ValueError:
+      continue
+  print(line + " " + str(j) + " " + str(partial_result))
 #Print result         
 print(result)
